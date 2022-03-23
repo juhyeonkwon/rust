17b5a3650a3e5e49dbba0896ffd0ad516ad150ee
kwonjuhyeon <asshole96@naver.com>
2022-03-23 22:44:46 +0900

    ,

--- gentraitlifetime/src/trait.rs
+++
@@ -1,9 +0,0 @@
-extern crate gentraitlifetime;
-
-fn main() {
-
-  let b = NewArticle{headline : String::from("dtd"), location : String::from("dtd"), author : String::from("dtd"), content : String::from("dtd")};
-
-  println!("{}",b.summary());
-
-}
---
+++ gentraitlifetime/trait.rs
@@ -0,0 +1,46 @@
+pub trait Summarizable {
+  fn author_summary(&self) -> String;
+
+  fn summary(&self) -> String {
+      format!("(Read more from {}...)", self.author_summary())
+  }
+}
+
+pub struct NewsArticle {
+  pub headline: String,
+  pub location: String,
+  pub author: String,
+  pub content: String,
+}
+
+
+pub struct Tweet {
+  pub username: String,
+  pub content: String,
+  pub reply: bool,
+  pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+  fn author_summary(&self) -> String {
+      format!("@{}", self.username)
+  }
+}
+
+
+pub fn notify<T: Summarizable>(item: T) {
+  println!("Breaking news! {}", item.summary());
+}
+
+fn main() {
+  let tweet = Tweet {
+    username: String::from("horse_ebooks"),
+    content: String::from("of course, as you probably already know, people"),
+    reply: false,
+    retweet: false,
+  };  
+
+  notify(tweet);
+
+
+}
